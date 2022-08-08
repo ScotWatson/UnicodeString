e3a3439fec8a0c71c2c6636f27d8c22628f0d49d
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-08-08 18:21:11 -0400

    Update UnicodeString.mjs

--- docs/UnicodeString.mjs
+++ docs/UnicodeString.mjs
@@ -78,7 +78,6 @@ mapEncoding.set("iso8859-6", "ISO_8859_6");
 mapEncoding.set("iso88596", "ISO_8859_6");
 mapEncoding.set("iso_8859-6", "ISO_8859_6");
 mapEncoding.set("iso_8859-6:1987", "ISO_8859_6");
-
 mapEncoding.set("csisolatingreek", "ISO_8859_7");
 mapEncoding.set("ecma-118", "ISO_8859_7");
 mapEncoding.set("elot_928", "ISO_8859_7");
@@ -91,70 +90,151 @@ mapEncoding.set("iso88597", "ISO_8859_7");
 mapEncoding.set("iso_8859-7", "ISO_8859_7");
 mapEncoding.set("iso_8859-7:1987", "ISO_8859_7");
 mapEncoding.set("sun_eu_greek", "ISO_8859_7");
-
-"csiso88598e", "csisolatinhebrew", "hebrew", "iso-8859-8", "iso-8859-8-e", "iso-ir-138", "iso8859-8", "iso88598", "iso_8859-8", "iso_8859-8:1988", "visual" 	iso-8859-8
-
-"csiso88598i", "iso-8859-8-i", "logical" 	iso-8859-8i
-
-"csisolatin6", "iso-8859-10", "iso-ir-157", "iso8859-10", "iso885910", "l6", "latin6" 	iso-8859-10
-
-"iso-8859-13", "iso8859-13", "iso885913" 	iso-8859-13
-
-"iso-8859-14", "iso8859-14", "iso885914" 	iso-8859-14
-
-"csisolatin9", "iso-8859-15", "iso8859-15", "iso885915", "l9", "latin9" 	iso-8859-15
-
-"iso-8859-16" 	iso-8859-16
-
-"cskoi8r", "koi", "koi8", "koi8-r", "koi8_r" 	koi8-r
-
-"koi8-u" 	koi8-u
-
-"csmacintosh", "mac", "macintosh", "x-mac-roman" 	macintosh
-
-"dos-874", "iso-8859-11", "iso8859-11", "iso885911", "tis-620", "windows-874" 	windows-874
-
-"cp1250", "windows-1250", "x-cp1250" 	windows-1250
-
-"cp1251", "windows-1251", "x-cp1251" 	windows-1251
-
-"ansi_x3.4-1968", "ascii", "cp1252", "cp819", "csisolatin1", "ibm819", "iso-8859-1", "iso-ir-100", "iso8859-1", "iso88591", "iso_8859-1", "iso_8859-1:1987", "l1", "latin1", "us-ascii", "windows-1252", "x-cp1252" 	windows-1252
-
-"cp1253", "windows-1253", "x-cp1253" 	windows-1253
-
-"cp1254", "csisolatin5", "iso-8859-9", "iso-ir-148", "iso8859-9", "iso88599", "iso_8859-9", "iso_8859-9:1989", "l5", "latin5", "windows-1254", "x-cp1254" 	windows-1254
-
-"cp1255", "windows-1255", "x-cp1255" 	windows-1255
-
-"cp1256", "windows-1256", "x-cp1256" 	windows-1256
-
-"cp1257", "windows-1257", "x-cp1257" 	windows-1257
-
-"cp1258", "windows-1258", "x-cp1258" 	windows-1258
-
-"x-mac-cyrillic", "x-mac-ukrainian" 	x-mac-cyrillic
-
-"chinese", "csgb2312", "csiso58gb231280", "gb2312", "gb_2312", "gb_2312-80", "gbk", "iso-ir-58", "x-gbk" 	gbk
-
-"gb18030" 	gb18030
-
-"hz-gb-2312" 	hz-gb-2312
-
-"big5", "big5-hkscs", "cn-big5", "csbig5", "x-x-big5" 	big5
-
-"cseucpkdfmtjapanese", "euc-jp", "x-euc-jp" 	euc-jp
-
-"csiso2022jp", "iso-2022-jp" 	iso-2022-jp
-
-"csshiftjis", "ms_kanji", "shift-jis", "shift_jis", "sjis", "windows-31j", "x-sjis" 	shift-jis
-
-"cseuckr", "csksc56011987", "euc-kr", "iso-ir-149", "korean", "ks_c_5601-1987", "ks_c_5601-1989", "ksc5601", "ksc_5601", "windows-949" 	euc-kr
-
-"csiso2022kr", "iso-2022-kr" 	iso-2022-kr
-
-"utf-16be" 	utf-16be
-
-"utf-16", "utf-16le" 	utf-16le
+mapEncoding.set("csiso88598e", "ISO_8859_8");
+mapEncoding.set("csisolatinhebrew", "ISO_8859_8");
+mapEncoding.set("hebrew", "ISO_8859_8");
+mapEncoding.set("iso-8859-8", "ISO_8859_8");
+mapEncoding.set("iso-8859-8-e", "ISO_8859_8");
+mapEncoding.set("iso-ir-138", "ISO_8859_8");
+mapEncoding.set("iso8859-8", "ISO_8859_8");
+mapEncoding.set("iso88598", "ISO_8859_8");
+mapEncoding.set("iso_8859-8", "ISO_8859_8");
+mapEncoding.set("iso_8859-8:1988", "ISO_8859_8");
+mapEncoding.set("visual", "ISO_8859_8");
+mapEncoding.set("csiso88598i", "ISO_8859_8i");
+mapEncoding.set("iso-8859-8-i", "ISO_8859_8i");
+mapEncoding.set("logical", "ISO_8859_8i");
+mapEncoding.set("csisolatin6", "ISO_8859_10");
+mapEncoding.set("iso-8859-10", "ISO_8859_10");
+mapEncoding.set("iso-ir-157", "ISO_8859_10");
+mapEncoding.set("iso8859-10", "ISO_8859_10");
+mapEncoding.set("iso885910", "ISO_8859_10");
+mapEncoding.set("l6", "ISO_8859_10");
+mapEncoding.set("latin6", "ISO_8859_10");
+mapEncoding.set("iso-8859-13", "ISO_8859_13");
+mapEncoding.set("iso8859-13", "ISO_8859_13");
+mapEncoding.set("iso885913", "ISO_8859_13");
+mapEncoding.set("iso-8859-14", "ISO_8859_14");
+mapEncoding.set("iso8859-14", "ISO_8859_14");
+mapEncoding.set("iso885914", "ISO_8859_14");
+mapEncoding.set("csisolatin9", "ISO_8859_15");
+mapEncoding.set("iso-8859-15", "ISO_8859_15");
+mapEncoding.set("iso8859-15", "ISO_8859_15");
+mapEncoding.set("iso885915", "ISO_8859_15");
+mapEncoding.set("l9", "ISO_8859_15");
+mapEncoding.set("latin9", "ISO_8859_15");
+mapEncoding.set("iso-8859-16", "ISO_8859_16");
+mapEncoding.set("cskoi8r", "koi8_r");
+mapEncoding.set("koi", "koi8_r");
+mapEncoding.set("koi8", "koi8_r");
+mapEncoding.set("koi8-r", "koi8_r");
+mapEncoding.set("koi8_r", "koi8_r");
+mapEncoding.set("koi8-u", "koi8_u");
+mapEncoding.set("csmacintosh", "macintosh");
+mapEncoding.set("mac", "macintosh");
+mapEncoding.set("macintosh", "macintosh");
+mapEncoding.set("x-mac-roman", "macintosh");
+mapEncoding.set("dos-874", "windows_874");
+mapEncoding.set("iso-8859-11", "windows_874");
+mapEncoding.set("iso8859-11", "windows_874");
+mapEncoding.set("iso885911", "windows_874");
+mapEncoding.set("tis-620", "windows_874");
+mapEncoding.set("windows-874", "windows_874");
+mapEncoding.set("cp1250", "windows_1250");
+mapEncoding.set("windows-1250", "windows_1250");
+mapEncoding.set("x-cp1250", "windows_1250");
+mapEncoding.set("cp1251", "windows_1251");
+mapEncoding.set("windows-1251", "windows_1251");
+mapEncoding.set("x-cp1251", "windows_1251");
+mapEncoding.set("ansi_x3.4-1968", "windows_1252");
+mapEncoding.set("ascii", "windows_1252");
+mapEncoding.set("cp1252", "windows_1252");
+mapEncoding.set("cp819", "windows_1252");
+mapEncoding.set("csisolatin1", "windows_1252");
+mapEncoding.set("ibm819", "windows_1252");
+mapEncoding.set("iso-8859-1", "windows_1252");
+mapEncoding.set("iso-ir-100", "windows_1252");
+mapEncoding.set("iso8859-1", "windows_1252");
+mapEncoding.set("iso88591", "windows_1252");
+mapEncoding.set("iso_8859-1", "windows_1252");
+mapEncoding.set("iso_8859-1:1987", "windows_1252");
+mapEncoding.set("l1", "windows_1252");
+mapEncoding.set("latin1", "windows_1252");
+mapEncoding.set("us-ascii", "windows_1252");
+mapEncoding.set("windows-1252", "windows_1252");
+mapEncoding.set("x-cp1252", "windows_1252");
+mapEncoding.set("cp1253", "windows_1253");
+mapEncoding.set("windows-1253", "windows_1253");
+mapEncoding.set("x-cp1253", "windows_1253");
+mapEncoding.set("cp1254", "windows_1254");
+mapEncoding.set("csisolatin5", "windows_1254");
+mapEncoding.set("iso-8859-9", "windows_1254");
+mapEncoding.set("iso-ir-148", "windows_1254");
+mapEncoding.set("iso8859-9", "windows_1254");
+mapEncoding.set("iso88599", "windows_1254");
+mapEncoding.set("iso_8859-9", "windows_1254");
+mapEncoding.set("iso_8859-9:1989", "windows_1254");
+mapEncoding.set("l5", "windows_1254");
+mapEncoding.set("latin5", "windows_1254");
+mapEncoding.set("windows-1254", "windows_1254");
+mapEncoding.set("x-cp1254", "windows_1254");
+mapEncoding.set("cp1255", "windows_1255");
+mapEncoding.set("windows-1255", "windows_1255");
+mapEncoding.set("x-cp1255", "windows_1255");
+mapEncoding.set("cp1256", "windows_1256");
+mapEncoding.set("windows-1256", "windows_1256");
+mapEncoding.set("x-cp1256", "windows_1256");
+mapEncoding.set("cp1257", "windows_1257");
+mapEncoding.set("windows-1257", "windows_1257");
+mapEncoding.set("x-cp1257", "windows_1257");
+mapEncoding.set("cp1258", "windows_1258");
+mapEncoding.set("windows-1258", "windows_1258");
+mapEncoding.set("x-cp1258", "windows_1258");
+mapEncoding.set("x-mac-cyrillic", "x_mac_cyrillic");
+mapEncoding.set("x-mac-ukrainian", "x_mac_cyrillic");
+mapEncoding.set("chinese", "gbk");
+mapEncoding.set("csgb2312", "gbk");
+mapEncoding.set("csiso58gb231280", "gbk");
+mapEncoding.set("gb2312", "gbk");
+mapEncoding.set("gb_2312", "gbk");
+mapEncoding.set("gb_2312-80", "gbk");
+mapEncoding.set("gbk", "gbk");
+mapEncoding.set("iso-ir-58", "gbk");
+mapEncoding.set("x-gbk", "gbk");
+mapEncoding.set("gb18030", "gb18030");
+mapEncoding.set("hz-gb-2312", "hz_gb_2312");
+mapEncoding.set("big5", "big5");
+mapEncoding.set("big5-hkscs", "big5");
+mapEncoding.set("cn-big5", "big5");
+mapEncoding.set("csbig5", "big5");
+mapEncoding.set("x-x-big5", "big5");
+mapEncoding.set("cseucpkdfmtjapanese", "euc_jp");
+mapEncoding.set("euc-jp", "euc_jp");
+mapEncoding.set("x-euc-jp", "euc_jp");
+mapEncoding.set("csiso2022jp", "ISO_2022_jp");
+mapEncoding.set("iso-2022-jp", "ISO_2022_jp");
+mapEncoding.set("csshiftjis", "shift_jis");
+mapEncoding.set("ms_kanji", "shift_jis");
+mapEncoding.set("shift-jis", "shift_jis");
+mapEncoding.set("shift_jis", "shift_jis");
+mapEncoding.set("sjis", "shift_jis");
+mapEncoding.set("windows-31j", "shift_jis");
+mapEncoding.set("x-sjis", "shift_jis");
+mapEncoding.set("cseuckr", "euc_kr");
+mapEncoding.set("csksc56011987", "euc_kr");
+mapEncoding.set("euc-kr", "euc_kr");
+mapEncoding.set("iso-ir-149", "euc_kr");
+mapEncoding.set("korean", "euc_kr");
+mapEncoding.set("ks_c_5601-1987", "euc_kr");
+mapEncoding.set("ks_c_5601-1989", "euc_kr");
+mapEncoding.set("ksc5601", "euc_kr");
+mapEncoding.set("ksc_5601", "euc_kr");
+mapEncoding.set("windows-949", "euc_kr");
+mapEncoding.set("csiso2022kr", "ISO_2022_kr");
+mapEncoding.set("iso-2022-kr", "ISO_2022_kr");
+mapEncoding.set("utf-16be", "UTF_16be");
+mapEncoding.set("utf-16", "UTF_16le");
+mapEncoding.set("utf-16le", "UTF_16le");
 
 
 function importEncodingModule(name) {
